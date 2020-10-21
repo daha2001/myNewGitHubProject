e2bdbeff40e8edb70e7d1ad5f849d765dfce2cae
Daniel Håkansson <daniel.hakansson@yh.nackademin.se>
2020-10-21 11:42:04 +0200

    testet

--- src/myNewGitHubProject.java
+++ src/myNewGitHubProject.java
@@ -2,5 +2,6 @@ public class myNewGitHubProject {
     public static void main(String[] args) {
 
         System.out.println("hellooooouu");
+        System.out.println("nästa rad - test");
     }
 }
